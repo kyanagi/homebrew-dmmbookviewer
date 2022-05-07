a406dcb8bb2b7300edd6d0f4e2bdf78fd2ebb65e
Kouhei Yanagita <yanagi@shakenbu.org>
2022-05-08 06:20:34 +0900

    Add dmmbookviewer.rb

---
+++ Casks/dmmbookviewer.rb
@@ -0,0 +1,20 @@
+cask "dmmbookviewer" do
+  version :latest
+  sha256 :no_check
+
+  url "https://dl.book.dmm.com/download/mac/DMMViewerSetup_Mac.pkg"
+  name "DMMbookviewer"
+  desc "Book viewer for the DMM Books platform"
+  homepage "https://book.dmm.com/info_bookviewer.html"
+
+  pkg "DMMViewerSetup_Mac.pkg"
+
+  uninstall pkgutil: "com.dmm.DMMbookviewer",
+            quit: "com.dmm.DMMbookviewer"
+
+  zap trash: [
+        '~/Library/Application Support/DMM/DMM Books',
+        '~/Library/Application Support/CloudDocs/session/containers/iCloud.com.dmm.DMMBooks.plist',
+        '~/Library/Saved Application State/com.dmm.DMMbookviewer.savedState',
+      ]
+end
